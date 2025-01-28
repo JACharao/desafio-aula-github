d1d6ff41455f0e0c77d369caf1297e8d68c98939
Joao Antonio <joaoantoniocharao1@gmail.com>
2025-01-28 19:50:02 -0300

    "teste"

---
+++ exercicios.js
@@ -0,0 +1,105 @@
+//let contador = 1;    
+
+//while(contador <= 10) {
+//    console.log(`Contador número ${contador}`);
+//    contador++;
+//}
+
+
+//let contador = 10;    
+
+//while(contador >= 0) {
+//    console.log(`Contador número ${contador}`);
+//    contador--;
+//}
+
+
+//let numero = prompt('Digite o número desejado');
+
+//while(numero >= 0) {
+//    console.log(`Lançamento do foguete em ${numero}`);
+//    numero--;
+//}
+
+
+//let contador = 0
+//let numeroMaximo = prompt('Digite o número desejado');
+
+//while(contador <= numeroMaximo) {
+//    console.log(`Desarmando a bomba em ${numeroMinimo} segundos`);
+//    numeroMinimo++;
+//}
+
+
+//console.log('Boas vindas de volta');
+
+
+//let nome = João;
+//console.log(`Olá, ${nome}!`);
+
+
+//let nome = João;
+//alert(`Olá, ${nome}!`);
+
+//let linguagemFavorita = prompt('Qual a linguagem de programação que você mais gosta?');
+//console.log(linguagemFavorita);
+
+//let valor1 = 7;
+//let valor2 = 4;
+//let resultado = valor1 + valor2;
+//console.log(`a soma de ${valor1} e ${valor2} é igual a ${resultado}`);
+
+
+//let valor1 = 7;
+//let valor2 = 4;
+//let resultado = valor1 - valor2;
+//console.log(`a diferença entre ${valor1} e ${valor2} é igual a ${resultado}`);
+
+
+//let idade = prompt('Qual a sua idade?')
+
+//if(idade > 17){
+//    console.log('O usuario é maior de idade')
+// } else {
+//    console.log('O usuario é menor de idade')
+// }
+
+//let numero = prompt('Digite um número negativo ou positivo')
+
+//if(numero > 0) {
+//    console.log('O número é positivo')
+//} else if(numero < 0) {
+//    console.log('O número é negativo')
+//} else {
+//    console.log('O número é 0')
+//}
+
+
+//let numero = 1
+
+//while(numero <= 10) {
+//    alert(`Número ${numero}`)
+//    numero++
+//}
+
+
+//let nota = 8
+
+//if(nota >= 7) {
+//    console.log('Aprovado')
+//} else {
+//    console.log('Reprovado')
+//}
+
+
+//numeroAleatorio = Math.random()
+//console.log(numeroAleatorio)
+
+
+//numeroAleatorio = parseInt(Math.random() * 10 + 1)
+//console.log(numeroAleatorio)
+
+
+//numeroAleatorio = parseInt(Math.random() * 1000 + 1)
+//console.log(numeroAleatorio)
+
